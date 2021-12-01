0dd7c39e95c743d5ddca79148fefe041259ca8b5
Davi Banfi Nogueira <davibanfi1579@outlook.com>
2021-12-01 10:28:52 -0300

    feat: add toggle counter function

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import classes from './Counter.module.css';
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 const Counter = () => {
+  const dispach = useDispatch()
   const counter = useSelector(state => state.counter)
+  const show = useSelector(state => state.showCounter)
 
-  const toggleCounterHandler = () => {};
+  const incrementHandler = () => {
+    dispach({ type: 'increment' })
+  };
+
+  const decrementHandler = () => {
+    dispach({ type: 'decrement' })
+  };
+
+  const increaseHandler = () => {
+    dispach({ type: 'increase', amount: 10 })
+  }
+
+  const toggleCounterHandler = () => {
+    dispach({ type: 'toggle' })
+  }
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <div>
+        <button onClick={incrementHandler}>increment</button>
+        <button onClick={increaseHandler}>increase by 10</button>
+        <button onClick={decrementHandler}>decrement</button>
+      </div>
     </main>
   );
 };
